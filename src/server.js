c21fe01c163aa9faf433399dc8b363bf45f7c727
Mun Mun <munmun@sgcodecampus.com>
2019-10-08 15:37:41 +0800

    Extended PDFkit with a table.js, created a PDF that mimics Paypal invoice

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const PDFDocument = require("pdfkit");
+const PDFDocument = require("./pdftable");
 const fs = require("fs");
 
 const createPDF = () => {
@@ -124,6 +124,129 @@ const createPDF = () => {
   return doc;
 };
 
+const createInvoicePDF = () => {
+  const doc = new PDFDocument({
+    size: "LETTER",
+    margins: { top: 80, bottom: 80, left: 50, right: 50 },
+    layout: "portrait",
+    info: {
+      Title: "Invoice 0009 - SGCC",
+      Author: "SG Code Campus"
+    },
+    permissions: {
+      printing: "highResolution",
+      modifying: false,
+      copying: false,
+      annotating: false,
+      fillingForms: false,
+      contentAccessibility: false,
+      documentAssembly: false
+    }
+  });
+
+  doc
+    .fontSize(23)
+    .fillColor("grey")
+    .text("INVOICE", {
+      characterSpacing: 0.8,
+      align: "right",
+      font: "Helvetica-Bold"
+    });
+
+  doc
+    .fillColor("black")
+    .fontSize(12)
+    .lineGap(3)
+    .font("src/Lato-Regular.woff")
+    .text("Misty")
+    .text("info@ceruleangym.com");
+
+  doc.rect(450, 105, 120, 20).fill("green");
+
+  doc
+    .font("Helvetica-Bold")
+    .fill("white")
+    .fontSize(11)
+    .text("Paid", 455, 110);
+
+  doc
+    .font("src/Lato-Regular.woff")
+    .fontSize(12)
+    .lineGap(0)
+    .fill("black")
+    .text("Invoice #:  0009", 400, 180, { align: "left" })
+    .text("Invoice Date:  Oct 1, 2019", { align: "left" })
+    .text("Due Date:  Oct 11, 2019", { align: "left" });
+
+  doc
+    .roundedRect(420, 250, 150, 50, 5)
+    .stroke("grey")
+    .lineWidth(2);
+
+  doc
+    .font("src/Lato-Regular.woff")
+    .fill("black")
+    .fontSize(12)
+    .text("Amount due:", 463, 260);
+
+  doc
+    .font("Helvetica-Bold")
+    .fontSize(13)
+    .fill("black")
+    .text("5000P", 478);
+
+  doc
+    .moveTo(30, 320)
+    .lineTo(580, 320)
+    .lineWidth(1)
+    .stroke("grey");
+
+  doc
+    .font("Helvetica")
+    .fontSize(13)
+    .fill("black")
+    .text("Bill To:", 50, 340)
+    .moveDown()
+    .font("src/Lato-Regular.woff")
+    .fontSize(12)
+    .text("Ash Ketchum")
+    .text("5, Pallet Town, Kanto")
+    .moveDown()
+    .text("forever10@gmail.com")
+    .text("+65 1234 1234");
+
+  const table0 = {
+    headers: ["Description", "Quantity", "Price", "Amount"],
+    rows: [["Give me back my bike money", "1", "5000P", "5000P"]]
+  };
+
+  doc.table(table0, 50, 480, {
+    prepareHeader: () => doc.font("Helvetica-Bold").fontSize(12),
+    prepareRow: () =>
+      doc
+        .font("src/Lato-Regular.woff")
+        .fontSize(12)
+        .fill("black")
+  });
+
+  const table1 = {
+    headers: ["", ""],
+    rows: [["Subtotal", "5000P"], ["Total", "5000P"]]
+  };
+
+  doc.table(table1, {
+    prepareRow: () =>
+      doc
+        .font("src/Lato-Regular.woff")
+        .fontSize(11)
+        .fill("black")
+  });
+
+  doc.end();
+
+  return doc;
+};
+
 // Root
 app.get("/", (req, res) => {
   res.send(`Hello world`);
@@ -138,14 +261,16 @@ app.get("/as-iframe", (req, res) => {
 app.get("/output", (req, res) => {
   res.contentType("application/pdf");
 
-  const doc = createPDF();
+  // const doc = createPDF();
+  const doc = createInvoicePDF();
 
   doc.pipe(res);
 });
 
 // Saves a copy onto server.
 app.get("/save-to-server", (req, res) => {
-  const doc = createPDF();
+  // const doc = createPDF();
+  const doc = createInvoicePDF();
 
   doc.pipe(fs.createWriteStream("output.pdf"));
 
